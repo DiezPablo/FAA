cbdb5d1f15600c3f38cc974cbe86cd97d4e8050a
Alejandro Alcala Alvarez <aalcala1997@gmail.com>
2019-09-23 14:37:32 +0200

    Terminado, mejorar codigo o encontrar fallos

--- Practica0/DatosTemplate.py
+++ Practica0/DatosTemplate.py
@@ -43,25 +43,30 @@ class Datos:
 
           #datos = f.readline().split(',')
           #print(datos)
-          #j = 0
-          #i = 0
+          j = 0
+          i = 0
           #k = 0
-          #valores = []
-          #dic = []
-          #for j in range(self.numAtributos):
-            #  for i in range(int(self.numDatos)):
-            #      if datosN[i][j] not in valores:
-            #          valores.append(datosN[i][j])
+          valores = []
+          dic = {}
+          for j in range(self.numAtributos):
+              for i in range(int(self.numDatos)):
+                  if datosN[i][j] not in valores:
+                    valores.append(datosN[i][j])
           #print(valores)
+          k = 0
+          for valor in valores:
+              dic.update({valor:k})
+              k = k+1
+          #print(dic)
           self.diccionarios = []
           i = 0
-          for i in range(self.numAtributos-1):
-              print(self.nominalAtributos[i])
+          for i in range(self.numAtributos):
+              #print(self.nominalAtributos[i])
               if self.nominalAtributos[i] == True:
-                  self.diccionarios.append({'x':0,'o':1,'b':2})
+                  self.diccionarios.append(dic)
               else:
                   self.diccionarios.append({})
-          self.diccionarios.append({'positive\r\n':1, 'negative\r\n':0})
+          #self.diccionarios.append({'positive\r\n':1, 'negative\r\n':0})
           self.datos = np.empty((int(self.numDatos),int(self.numAtributos)))
           print(self.datos.shape)
           i = 0
