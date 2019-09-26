eceed410e5a3ab7d59ffb0af88280948c0cc6bed
DiezPablo <pablo.diezd@estudiante.uam.es>
2019-09-26 18:16:44 +0200

    hecho a falta de controlar el tema valores continuos o nominales

--- Practica0/DatosTemplate.py
+++ Practica0/DatosTemplate.py
@@ -34,50 +34,49 @@ class Datos:
                 self.nominalAtributos.append(True)
         print(self.nominalAtributos)
 
-
-        # Creacion de lista diccionarios, en caso de que el atributo sea Continuo, el diccionario estara vacio
-        self.listaDicts = []
-        for i in range(len(self.tipoAtributos)):
-            self.listaDicts.append({})
-        print(self.listaDicts)
-
+        # Guardamos los datos del fichero y los formateamos, de tal forma que cada linea es una lista
         datos = f.readlines()
         datosFormat = []
         for lista in datos:
             datosFormat.append(lista.strip('\n').split(','))
 
         # print(set(sorted(datosFormat[0])))
-        atributo = []
+        listaDatosAtributos = []
         for i in range(len(self.tipoAtributos)):
-            atributo.append([])
+            listaDatosAtributos.append([])
 
-        #print(atributo)
-
+        # Hacemos la traspuesta de los datos que guardamos para que cada lista de atributo guarde todos los datos
+        # de cada atributo.
         for lista in datosFormat:
             i = 0
             for item in lista:
-                atributo[i].append(item)
+                listaDatosAtributos[i].append(item)
                 i += 1
-        atributos = []
+
+        # Ordenamos y hacemos un set para eliminar repetidos.
+        i = 0
+        for item in listaDatosAtributos:
+            listaDatosAtributos[i] = sorted(set(item))
+            i += 1
+        #print(listaDatosAtributos)
+
+
+        # Creacion de lista diccionarios, en caso de que el atributo sea Continuo, el diccionario estara vacio
+        self.listaDicts = []
         for i in range(len(self.tipoAtributos)):
-            atributos.append([])
-        i = 0
-        for item2 in atributo:
-            atributos[i].append(sorted(set(item2)))
-            i += 1
+            self.listaDicts.append({})
 
+        # Creamos el diccionario tal y como se describe en las diapositivas, por orden y asignando valores numericos crecientes
         i = 0
-        j = 0
-        m = 0
-        for aux in atributos:
+        for atributo in listaDatosAtributos:
             k = 0
-            if self.tipoAtributos[i] == 'Nominal':
-                for j in range(len(aux)):
-                    for m in range(len(aux[j])):
-                        self.listaDicts[i].update({aux[j][m]:k})
-                        k += 1
+            for dato in atributo:
+                self.listaDicts[i][dato] = k
+                k += 1
             i += 1
+        print(self.listaDicts)
 
+        # Creacion de la matriz de datos utilizando el diccionario para mapear los valores
         self.datos = np.empty((int(self.numDatos),int(len(self.tipoAtributos))))
         i = 0
         j = 0
@@ -88,7 +87,7 @@ class Datos:
         #print(atributo)
         #print(atributos)
         #print(self.listaDicts)
-        print(self.datos[0])
+        print(self.datos)
 
 
 
