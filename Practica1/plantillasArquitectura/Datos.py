b11b9159e06c7a55228dd2ed4e46fdf404af3887
DiezPablo <pablo.diezd@estudiante.uam.es>
2019-10-02 15:38:52 +0200

    Añadiendo plantillas P1

---
+++ Practica1/plantillasArquitectura/Datos.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class Datos:
+  
+  TiposDeAtributos=('Continuo','Nominal')
+ 
+  # TODO: procesar el fichero para asignar correctamente las variables tipoAtributos, nombreAtributos, nominalAtributos, datos y diccionarios
+  # NOTA: No confundir TiposDeAtributos con tipoAtributos
+  def __init__(self, nombreFichero):
+    
+  # TODO: implementar en la práctica 1
+  def extraeDatos(self, idx):
+    pass
+
+
+  
