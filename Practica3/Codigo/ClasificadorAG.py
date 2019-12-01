f52172aea0ae722a1d53d593e28c3575f14cf04d
DiezPablo <pablo.diezpozo@gmail.com>
2019-12-01 20:15:29 +0100

    Generacion de reglas

--- Practica3/Codigo/ClasificadorAG.py
+++ Practica3/Codigo/ClasificadorAG.py
@@ -17,17 +17,35 @@ class ClasificadorAlgoritmoGenetico(Clasificador):
     self.individuos = {}
 
     # Calculamos la longitud de la regla que se va a generar y los intervalos dentro la regla que hacen referencia a cada atributo.
-    self.listaDictsIntervalos = {}
+    self.listaDictsIntervalos = np.array([])
 
+    longitud_regla = 0
     for i in range(len(dataset.listaDicts)):
+      dict ={}
+      dict['inicio'] = longitud_regla
+      dict['final'] = longitud_regla + len(dataset.listaDicts[i]) -1
+      self.listaDictsIntervalos = np.append(self.listaDictsIntervalos,dict)
 
+      longitud_regla += len(dataset.listaDicts[i])
 
+    self.generar_regla(longitud_regla)
 
+    # Falta generar las reglas!!!
 
   def generar_regla(self, longitud_regla):
 
-    i = 0
-    print(i)
+    # Generamos una regla de longitud calculada anteriormente
+    regla = np.zeros(longitud_regla)
+
+    for i in range(len(self.listaDictsIntervalos)):
+      aleat = np.random.randint(low = self.listaDictsIntervalos[i]['inicio'], high = self.listaDictsIntervalos[i]['final'] + 1)
+      regla[aleat] = 1
+
+    return regla
+
+
+
+
 
 
 
